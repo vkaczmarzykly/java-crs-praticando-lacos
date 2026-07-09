12121c8e7dd23fb0408668b38981b0da8dc33e1b
Vitor Lyra <vkaczmarzykly@gmail.com>
2026-07-09 15:29:12 -0300

    feature/contagem-degraus feat: add exercise Contagem de Degraus.

--- src/ContagemDegraus.java
+++ src/ContagemDegraus.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class ContagemDegraus {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a quantidade de degraus: ");
+        int qtdeDegraus = scanner.nextInt();
+
+        for (int i = 1; i <= qtdeDegraus; i++) {
+            System.out.println("Subindo degrau " + i);
+        }
+        System.out.println("Você chegou ao topo!");
+        scanner.close();
+    }
 }
